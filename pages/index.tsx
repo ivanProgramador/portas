0ddf44d0f963f2e7a5b2901c7f096451714fc396
Ivan Rodrigues <ivan-guita@hotmail.com>
2024-10-08 21:46:16 -0300

    classe porta concluida , componente porta concluido

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import Porta from "../components/Porta";
 
 export default function Home() {
   return (
-    <>
+    <div style={{display:"flex"}} >
+      <Porta selecionada={true} />
       <Porta />
-    </>
+    </div>
   );
 }
